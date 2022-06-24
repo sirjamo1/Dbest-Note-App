970e30be290dfb288f9af6a4f5dc4974baf60ecf
Sirjamo1 <jam_how@hotmail.com>
2022-06-25 06:56:12 +1000

    *fixed note title displaying multiply lines

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -9,9 +9,11 @@ export default function Sidebar(props) {
         }`}
         onClick={() => props.setCurrentNoteId(note.id)}
       >
-        {/* the "/n" didn't seem to exist in note.body so i changed it to look for the first 30 char */}
-        <h4 className="text-snippet">{note.body.split("", 30)}</h4>
-        <button className="delete-btn" onClick={(event) => props.deleteNote(event, note.id)}>
+        <h4 className="text-snippet">{note.body.split("\n")[0]}</h4>
+        <button
+          className="delete-btn"
+          onClick={(event) => props.deleteNote(event, note.id)}
+        >
           <i className="gg-trash trash-icon"></i>
         </button>
       </div>
