2bf8488a6d18766a377680e2b6421c0982d4b2e4
Sirjamo1 <jam_how@hotmail.com>
2022-07-05 11:14:33 +1000

    Added logout link to navbar for when user is logged in

--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -1,17 +1,23 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import { useAuth } from "../auth";
+import { useNavigate } from "react-router-dom";
 import "./NavBar.css";
 import pencil from "../images/pencil.png";
 
 export const Navbar = () => {
+  const auth = useAuth();
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    auth.logout();
+    navigate("/");
+  };
   const navLinkStyles = ({ isActive }) => {
     return {
       fontWeight: isActive ? "bold" : "normal",
       textDecoration: isActive ? "none" : "underline",
     };
   };
-  const auth = useAuth();
   return (
     <nav className="primary--nav">
       <NavLink style={navLinkStyles} to="/">
@@ -27,10 +33,18 @@ export const Navbar = () => {
         Profile
       </NavLink>
       {!auth.user && (
-        <NavLink style={navLinkStyles} to="/Login">
+        <NavLink style={navLinkStyles} to="/login">
           <div className="pencil--container">
             <img className="nav--pencil" src={pencil} alt="pencil"></img>
             <div className="signInUp">Log In</div>
+          </div>
+        </NavLink>
+      )}
+      {auth.user && (
+        <NavLink style={navLinkStyles} to="/">
+          <div onClick={handleLogout} className="pencil--container">
+            <img className="nav--pencil" src={pencil} alt="pencil"></img>
+            <div className="signInUp">Log out</div>
           </div>
         </NavLink>
       )}
