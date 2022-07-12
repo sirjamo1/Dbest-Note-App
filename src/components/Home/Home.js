55ee06b25499544a466b02fbf0aa4130a39fc287
Sirjamo1 <jam_how@hotmail.com>
2022-07-12 14:14:58 +1000

    updated readme

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,26 +1,29 @@
 import React from "react";
-import "./Home.css"
+import "./Home.css";
 import { Link } from "react-router-dom";
 
 export const Home = () => {
-  //need to add welcome message/link to sign in page
-  const userEmail = sessionStorage.getItem("email")
-  const linkStyle = {
-    textDecoration: "none"
-  }
-  return (
-      <div className="home-post-it-container">
-          <h1>Welcome to Notes</h1>
-          <div className="home-post-it-card">
-              <h2>Welcome</h2>
-              <h4>
-                  {userEmail ? (
-                      userEmail
-                  ) : (
-                      <Link style={linkStyle} to="/login"> Please Login</Link>
-                  )}
-              </h4>
-          </div>
-      </div>
-  );
+    //need to add welcome message/link to sign in page
+    const userEmail = sessionStorage.getItem("email");
+    const linkStyle = {
+        textDecoration: "none",
+    };
+    return (
+        <div className="home-post-it-container">
+            <h1>Welcome to Notes</h1>
+            <div className="home-post-it-card">
+                <h2>Welcome</h2>
+                <h4>
+                    {userEmail ? (
+                        userEmail
+                    ) : (
+                        <Link style={linkStyle} to="/login">
+                            {" "}
+                            Please Login
+                        </Link>
+                    )}
+                </h4>
+            </div>
+        </div>
+    );
 };
